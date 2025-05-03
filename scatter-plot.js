ea0d9a117feca3b336e4009523c70a4b5fb3a20c
Sai swaroop <104573154+swaroop9010@users.noreply.github.com>
2025-05-02 23:08:26 -0400

    Update scatter-plot.js

--- scatter-plot.js
+++ scatter-plot.js
@@ -5,21 +5,22 @@ function updateScatterPlot(data) {
   scatterOriginalData = data;
   d3.select("#scatterPlot").select("svg").remove();
 
-  // Filter valid data
-  data = data.filter(d => d.Horsepower && d.Weight)
+  data = data.filter(d => d.Horsepower && d.Weight && d.Origin)
              .map(d => ({
                ...d,
                Horsepower: +d.Horsepower,
                Weight: +d.Weight
              }));
 
-  const margin = { top: 20, right: 30, bottom: 50, left: 60 },
+  const margin = { top: 20, right: 120, bottom: 50, left: 60 },
         width = 800 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
+  const color = d3.scaleOrdinal(d3.schemeCategory10);
+
   scatterSVG = d3.select("#scatterPlot")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
+    .attr("width", width + margin.left + margin.right + 100)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
@@ -32,29 +33,45 @@ function updateScatterPlot(data) {
               .domain(d3.extent(data, d => d.Horsepower))
               .range([height, 0]);
 
-  // X Axis
   scatterSVG.append("g")
     .attr("transform", `translate(0,${height})`)
     .call(d3.axisBottom(x));
+  scatterSVG.selectAll("path.domain").attr("stroke", "black");
+  scatterSVG.selectAll(".tick line").attr("stroke", "black");
+  scatterSVG.selectAll(".tick text").attr("fill", "black");
 
-  // Y Axis
   scatterSVG.append("g")
     .call(d3.axisLeft(y));
+  scatterSVG.selectAll("path.domain").attr("stroke", "black");
+  scatterSVG.selectAll(".tick line").attr("stroke", "black");
+  scatterSVG.selectAll(".tick text").attr("fill", "black");
 
-  // Circles
+  // Plot circles colored by origin
   scatterSVG.selectAll("circle")
     .data(data)
     .enter().append("circle")
     .attr("cx", d => x(d.Weight))
     .attr("cy", d => y(d.Horsepower))
     .attr("r", 5)
-    .attr("fill", "green")
+    .attr("fill", d => color(d.Origin))
     .attr("opacity", 0.7)
     .on("click", (event, d) => handleInteraction(d))
     .append("title")
-    .text(d => `${d.Car}\nWeight: ${d.Weight} lbs\nHP: ${d.Horsepower}`);
+    .text(d => `${d.Car}\nWeight: ${d.Weight} lbs\nHP: ${d.Horsepower}\nOrigin: ${d.Origin}`);
 
-  scatterSVG.selectAll("path.domain").attr("stroke", "black");
-  scatterSVG.selectAll(".tick line").attr("stroke", "black");
-  scatterSVG.selectAll(".tick text").attr("fill", "black");
+  // Legend
+  const origins = Array.from(new Set(data.map(d => d.Origin)));
+  const legend = scatterSVG.append("g")
+    .attr("transform", `translate(${width + 30}, 20)`);
+
+  origins.forEach((origin, i) => {
+    const row = legend.append("g").attr("transform", `translate(0, ${i * 20})`);
+    row.append("rect").attr("width", 15).attr("height", 15).attr("fill", color(origin));
+    row.append("text")
+      .attr("x", 20)
+      .attr("y", 12)
+      .text(origin)
+      .style("font-size", "12px")
+      .attr("fill", "black");
+  });
 }
