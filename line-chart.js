8427695171472acec0a0cb64a14325ab07db5feb
Sai swaroop <104573154+swaroop9010@users.noreply.github.com>
2025-05-02 22:57:54 -0400

    Update line-chart.js

--- line-chart.js
+++ line-chart.js
@@ -10,7 +10,6 @@ function updateLineChart(data) {
     d.MPG = +d.MPG;
   });
 
-  // Group by Origin and Year, compute avg MPG
   const groupedData = Array.from(
     d3.group(data, d => d.Origin),
     ([origin, values]) => ({
@@ -24,21 +23,18 @@ function updateLineChart(data) {
     })
   );
 
-  const margin = { top: 40, right: 80, bottom: 50, left: 60 },
+  const margin = { top: 40, right: 120, bottom: 50, left: 60 },
         width = 800 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
   lineSVG = d3.select("#lineChart")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
+    .attr("width", width + margin.left + margin.right + 100)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  const x = d3.scaleLinear()
-              .domain([70, 82])
-              .range([0, width]);
-
+  const x = d3.scaleLinear().domain([70, 82]).range([0, width]);
   const y = d3.scaleLinear()
               .domain([0, d3.max(groupedData, g => d3.max(g.values, d => d.avgMPG))])
               .range([height, 0]);
@@ -49,15 +45,23 @@ function updateLineChart(data) {
   lineSVG.append("g")
     .attr("transform", `translate(0,${height})`)
     .call(d3.axisBottom(x).tickFormat(d => 1900 + d));
+  lineSVG.selectAll("path.domain").attr("stroke", "black");
+  lineSVG.selectAll(".tick line").attr("stroke", "black");
+  lineSVG.selectAll(".tick text").attr("fill", "black");
 
   // Y Axis
-  lineSVG.append("g").call(d3.axisLeft(y));
+  lineSVG.append("g")
+    .call(d3.axisLeft(y));
+  lineSVG.selectAll("path.domain").attr("stroke", "black");
+  lineSVG.selectAll(".tick line").attr("stroke", "black");
+  lineSVG.selectAll(".tick text").attr("fill", "black");
 
   // Line generator
   const line = d3.line()
     .x(d => x(d.year))
     .y(d => y(d.avgMPG));
 
+  // Draw each origin line
   groupedData.forEach(group => {
     lineSVG.append("path")
       .datum(group.values)
@@ -66,16 +70,26 @@ function updateLineChart(data) {
       .attr("stroke-width", 2)
       .attr("d", line)
       .on("click", () => handleInteraction({ Origin: group.origin }));
-
-    lineSVG.append("text")
-      .attr("x", width - 60)
-      .attr("y", y(group.values[group.values.length - 1].avgMPG))
-      .attr("fill", color(group.origin))
-      .text(group.origin)
-      .style("font-size", "12px");
   });
 
-  lineSVG.selectAll("path.domain").attr("stroke", "black");
-  lineSVG.selectAll(".tick line").attr("stroke", "black");
-  lineSVG.selectAll(".tick text").attr("fill", "black");
+  // Legend
+  const legend = lineSVG.append("g")
+    .attr("transform", `translate(${width + 20}, 10)`);
+
+  groupedData.forEach((group, i) => {
+    const legendRow = legend.append("g")
+      .attr("transform", `translate(0, ${i * 20})`);
+
+    legendRow.append("rect")
+      .attr("width", 15)
+      .attr("height", 15)
+      .attr("fill", color(group.origin));
+
+    legendRow.append("text")
+      .attr("x", 20)
+      .attr("y", 12)
+      .text(group.origin)
+      .style("font-size", "12px")
+      .attr("fill", "black");
+  });
 }
