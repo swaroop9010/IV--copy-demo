c3dd132a9c9df03f5cb5f356783af64058986681
Sai swaroop <104573154+swaroop9010@users.noreply.github.com>
2025-05-02 22:15:38 -0400

    Update scatter-plot.js

--- scatter-plot.js
+++ scatter-plot.js
@@ -5,14 +5,19 @@ function updateScatterPlot(data) {
   scatterOriginalData = data;
   d3.select("#scatterPlot").select("svg").remove();
 
-  data = data.filter(d => d.MPG && d.Horsepower);
-  data.forEach(d => {
+  // Get top 20 cars by MPG
+  const topCars = [...data]
+    .filter(d => d.MPG && d.Horsepower && d.Car)
+    .sort((a, b) => +b.MPG - +a.MPG)
+    .slice(0, 20);
+
+  topCars.forEach(d => {
     d.Horsepower = +d.Horsepower;
     d.MPG = +d.MPG;
   });
 
-  const margin = {top: 20, right: 30, bottom: 50, left: 60},
-        width = 800 - margin.left - margin.right,
+  const margin = {top: 20, right: 30, bottom: 120, left: 60},
+        width = 900 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
   scatterSVG = d3.select("#scatterPlot")
@@ -22,41 +27,42 @@ function updateScatterPlot(data) {
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  const x = d3.scaleLinear()
-              .domain(d3.extent(data, d => d.Horsepower))
+  const x = d3.scalePoint()
+              .domain(topCars.map(d => d.Car))
               .range([0, width]);
 
   const y = d3.scaleLinear()
-              .domain(d3.extent(data, d => d.MPG))
+              .domain([0, d3.max(topCars, d => d.Horsepower)])
               .range([height, 0]);
 
   // X Axis
   scatterSVG.append("g")
-     .attr("transform", `translate(0,${height})`)
-     .attr("class", "axis")
-     .call(d3.axisBottom(x));
+    .attr("transform", `translate(0,${height})`)
+    .attr("class", "axis")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "rotate(-45)")
+    .style("text-anchor", "end")
+    .style("font-size", "10px");
 
   // Y Axis
   scatterSVG.append("g")
-     .attr("class", "axis")
-     .call(d3.axisLeft(y));
+    .attr("class", "axis")
+    .call(d3.axisLeft(y));
 
-  // Data points
+  // Dots
   scatterSVG.selectAll("circle")
-    .data(data)
+    .data(topCars)
     .enter().append("circle")
-    .attr("cx", d => x(d.Horsepower))
-    .attr("cy", d => y(d.MPG))
-    .attr("r", 4)
-    .attr("fill", "steelblue")
-    .on("click", (event, d) => {
-      handleInteraction(d);
-    })
+    .attr("cx", d => x(d.Car))
+    .attr("cy", d => y(d.Horsepower))
+    .attr("r", 5)
+    .attr("fill", "green")
+    .on("click", (event, d) => handleInteraction(d))
     .append("title")
-    .text(d => `${d.Car} (${d.MPG} MPG)`);
+    .text(d => `${d.Car} – ${d.Horsepower} HP`);
 
-  // Set axis color to black
   scatterSVG.selectAll(".axis path, .axis line, .axis text")
-     .attr("stroke", "black")
-     .attr("fill", "black");
+    .attr("stroke", "black")
+    .attr("fill", "black");
 }
