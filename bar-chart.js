b0c1e88f7e79c0279edda05a4e6724b7d6236659
Sai swaroop <104573154+swaroop9010@users.noreply.github.com>
2025-05-02 22:32:53 -0400

    Update bar-chart.js

--- bar-chart.js
+++ bar-chart.js
@@ -5,16 +5,16 @@ function updateBarChart(data) {
   barOriginalData = data;
   d3.select("#barChart").select("svg").remove();
 
-  // Count unique years per manufacturer
-  const manufacturerYears = d3.rollups(
+  const mpgByOrigin = d3.rollups(
     data,
-    v => new Set(v.map(d => d["Model Year"])).size,
-    d => d.Manufacturer
-  ).sort((a, b) => d3.descending(a[1], b[1]));
+    v => d3.mean(v, d => +d.MPG),
+    d => d.Origin
+  ).map(([Origin, AvgMPG]) => ({ Origin, AvgMPG }))
+   .sort((a, b) => d3.descending(a.AvgMPG, b.AvgMPG));
 
-  const margin = {top: 20, right: 30, bottom: 120, left: 60},
-        width = 900 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+  const margin = { top: 20, right: 30, bottom: 40, left: 60 },
+        width = 500 - margin.left - margin.right,
+        height = 350 - margin.top - margin.bottom;
 
   barSVG = d3.select("#barChart")
     .append("svg")
@@ -24,42 +24,34 @@ function updateBarChart(data) {
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
   const x = d3.scaleBand()
-              .domain(manufacturerYears.map(d => d[0]))
+              .domain(mpgByOrigin.map(d => d.Origin))
               .range([0, width])
-              .padding(0.2);
+              .padding(0.4);
 
   const y = d3.scaleLinear()
-              .domain([0, d3.max(manufacturerYears, d => d[1])])
+              .domain([0, d3.max(mpgByOrigin, d => d.AvgMPG)])
               .range([height, 0]);
 
   // X Axis
   barSVG.append("g")
     .attr("transform", `translate(0,${height})`)
-    .attr("class", "axis")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "rotate(-45)")
-    .style("text-anchor", "end");
+    .call(d3.axisBottom(x));
 
   // Y Axis
   barSVG.append("g")
-    .attr("class", "axis")
     .call(d3.axisLeft(y));
 
   // Bars
   barSVG.selectAll("rect")
-    .data(manufacturerYears)
+    .data(mpgByOrigin)
     .enter().append("rect")
-    .attr("x", d => x(d[0]))
-    .attr("y", d => y(d[1]))
+    .attr("x", d => x(d.Origin))
+    .attr("y", d => y(d.AvgMPG))
     .attr("width", x.bandwidth())
-    .attr("height", d => height - y(d[1]))
+    .attr("height", d => height - y(d.AvgMPG))
     .attr("fill", "#ff7f0e")
-    .on("click", (event, d) => {
-      handleInteraction({ Manufacturer: d[0] });
-    });
+    .on("click", (event, d) => handleInteraction({ Origin: d.Origin }));
 
-  // Axis styling
   barSVG.selectAll(".axis path, .axis line, .axis text")
     .attr("stroke", "black")
     .attr("fill", "black");
