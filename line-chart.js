15d23c3738b6ced59eb8808439f77c873bd0fea2
Sai swaroop <104573154+swaroop9010@users.noreply.github.com>
2025-05-03 21:34:47 -0400

    Update line-chart.js

--- line-chart.js
+++ line-chart.js
@@ -23,8 +23,8 @@ function updateLineChart(data) {
     })
   );
 
-  const margin = { top: 40, right: 80, bottom: 50, left: 60 },
-        width = 450 - margin.left - margin.right,
+  const margin = { top: 20, right: 130, bottom: 50, left: 60 },
+        width = 540 - margin.left - margin.right,
         height = 300 - margin.top - margin.bottom;
 
   lineSVG = d3.select("#lineChart")
@@ -44,18 +44,17 @@ function updateLineChart(data) {
   // X Axis
   lineSVG.append("g")
     .attr("transform", `translate(0,${height})`)
-    .call(d3.axisBottom(x).tickFormat(d => 1900 + d));
-  lineSVG.selectAll("path.domain").attr("stroke", "black");
-  lineSVG.selectAll(".tick line").attr("stroke", "black");
-  lineSVG.selectAll(".tick text").attr("fill", "black");
+    .call(d3.axisBottom(x).tickFormat(d => 1900 + d))
+    .selectAll("text")
+    .attr("fill", "black");
 
   // Y Axis
   lineSVG.append("g")
-    .call(d3.axisLeft(y));
-  lineSVG.selectAll("path.domain").attr("stroke", "black");
-  lineSVG.selectAll(".tick line").attr("stroke", "black");
-  lineSVG.selectAll(".tick text").attr("fill", "black");
+    .call(d3.axisLeft(y))
+    .selectAll("text")
+    .attr("fill", "black");
 
+  // Line Generator
   const line = d3.line()
     .x(d => x(d.year))
     .y(d => y(d.avgMPG));
@@ -65,18 +64,18 @@ function updateLineChart(data) {
       .datum(group.values)
       .attr("fill", "none")
       .attr("stroke", color(group.origin))
-      .attr("stroke-width", 2)
+      .attr("stroke-width", 3)
       .attr("d", line)
       .on("click", () => handleInteraction({ Origin: group.origin }));
   });
 
+  // Legend at Top Right
   const legend = lineSVG.append("g")
-    .attr("transform", `translate(${width - 40}, 10)`);
+    .attr("transform", `translate(${width - 10}, 0)`);
 
   groupedData.forEach((group, i) => {
-    const legendRow = legend.append("g")
-      .attr("transform", `translate(0, ${i * 20})`);
-    legendRow.append("rect").attr("width", 15).attr("height", 15).attr("fill", color(group.origin));
-    legendRow.append("text").attr("x", 20).attr("y", 12).text(group.origin).style("font-size", "12px").attr("fill", "black");
+    const row = legend.append("g").attr("transform", `translate(0, ${i * 20})`);
+    row.append("rect").attr("width", 15).attr("height", 15).attr("fill", color(group.origin));
+    row.append("text").attr("x", 20).attr("y", 12).text(group.origin).style("font-size", "12px").attr("fill", "black");
   });
 }
